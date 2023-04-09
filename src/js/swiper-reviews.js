01708bb4816ae8f513d31f3e65519784a77cd816
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2023-04-09 20:11:31 +0200

    reviews

--- src/js/swiper-reviews.js
+++ src/js/swiper-reviews.js
@@ -6,18 +6,14 @@ new Swiper('.reviews-slider', {
   grabCursor: true,
   slidesPerView: 1,
   spaceBetween: 10,
-  // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
     375: {
       slidesPerView: 1,
     },
-    // when window width is >= 480px
     768: {
       slidesPerView: 2,
       spaceBetween: 16
     },
-    // when window width is >= 640px
     1200: {
       slidesPerView: 3,
       spaceBetween: 28
