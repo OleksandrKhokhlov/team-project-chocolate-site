96f302e4959ffe2734af268de734178dd02b623c
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-04-11 09:16:02 +0300

    buy-now

--- src/js/modal-form-2.js
+++ src/js/modal-form-2.js
@@ -1,13 +1,13 @@
 (() => {
-  const refs = {
+  const refs2 = {
     openModalBtn: document.querySelector('[data-modal-open-2]'),
     closeModalBtn: document.querySelector('[data-modal-close-2]'),
     modal: document.querySelector('[data-modal-2]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs2.openModalBtn.addEventListener('click', toggleModal);
+  refs2.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden-2');
+    refs2.modal.classList.toggle('is-hidden-2');
   }
 })();
