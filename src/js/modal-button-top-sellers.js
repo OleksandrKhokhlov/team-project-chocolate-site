abb6d9c6452e588a76824659e90bb9039c63c9de
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-04-11 17:56:10 +0300

    modal

--- src/js/modal-button-top-sellers.js
+++ src/js/modal-button-top-sellers.js
@@ -1,12 +1,10 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-sellers-open]'),
-    closeModalBtn: document.querySelector('[data-sellers-close]'),
     modal: document.querySelector('[data-sellers-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
